708eb96e294930478035f242c49ca5ed25ad4043
yueliliu <yueliliu@YUELILIU-CN.oradev.oraclecorp.com>
2018-02-09 17:01:33 +0800

    add nike.bat

---
+++ drivers/nike.bat
@@ -0,0 +1,7 @@
+SET product=sb-air-force-2-low-kevin-bradley
+SET hour=16
+SET min=59
+start cmd /k "jdk1.8.0_76\jre\bin\java -jar sneakers-1.0.jar %product% 42 %hour% %min% juneslayer@gmail.com"
+start cmd /k "jdk1.8.0_76\jre\bin\java -jar sneakers-1.0.jar %product% 43 %hour% %min% juneslayer_0@gmail.com"
+start cmd /k "jdk1.8.0_76\jre\bin\java -jar sneakers-1.0.jar %product% 44 %hour% %min% 121225135@qq.com"
+start cmd /k "jdk1.8.0_76\jre\bin\java -jar sneakers-1.0.jar %product% 41 %hour% %min% lyly19821225@sina.com"
